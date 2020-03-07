85db86c633e224cb7f1d75f0b5b5ed556722bba7
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-03-07 05:18:43 -0600

    fixed the Api to have parametres and also need to fix the search button , add 15 car models, and soem minior styling left

--- GifTastic.js
+++ GifTastic.js
@@ -1,33 +1,59 @@
-var buttons=["Bently", "Benz", "BMW", "Porche", "Audi", "Range Rover","Lambo", "Ferrari","GT-R",]
+var buttons = ["Bently", "Benz", "BMW", "Porche", "Audi", "Range Rover", "Lambo", "Ferrari", "GT-R"]
 // created a display function and 4 loop
-function display(){
+function display() {
 
 
-    for (let index = 0; index < buttons.length; index++) { 
-           var button =  $("<button>") 
-           button.text(buttons[index])
-           button.addClass("carsBtn ml-1")
-           $(".buttonsDiv").append(button)
+    $(".buttonsDiv").empty()
+    for (let index = 0; index < buttons.length; index++) {
+        var button = $("<button>")
+        button.text(buttons[index])
+        button.addClass("carsBtn ml-1")
+        $(".buttonsDiv").append(button)
     }
     //crerated om click for all image buttons
 
-    $(".carsBtn").on("click",function(){
+    $(".carsBtn").on("click", function () {
 
         //grabs current text button clicked on page
-        var getTextBtn=$(this).text()
-        console.log("http://api.giphy.com/v1/gifs/search?q="+getTextBtn+"&api_key=DNSsuC2Sr861ml9NjnXxNjYmyA8iNUva")
+        var getTextBtn = $(this).text()
+        console.log("https://api.giphy.com/v1/gifs/search?q=" + getTextBtn +  "&api_key=DNSsuC2Sr861ml9NjnXxNjYmyA8iNUva&q=cars&limit=10&offset=0&rating=G&lang=en")
         //AJAX is a way to call API
         $.ajax({
-              url:"http://api.giphy.com/v1/gifs/search?q="+getTextBtn+"&api_key=DNSsuC2Sr861ml9NjnXxNjYmyA8iNUva",
-             method:"GET"
-            }).then(function(data){
-                console.log(data)
+
+            url: "https://api.giphy.com/v1/gifs/search?q=" + getTextBtn + "&api_key=DNSsuC2Sr861ml9NjnXxNjYmyA8iNUva&q=cars&limit=10&offset=0&rating=G&lang=en",
+            // url: "https://api.giphy.com/v1/gifs/search?q=" + getTextBtn + "&api_key=DNSsuC2Sr861ml9NjnXxNjYmyA8iNUva",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
             // creating a varrible for the rows and colloms dynamiclly using JQuery
 
-            //creating row tag
-            var row=$("<row>")
+            var images = response.data
+            /// empties out the place holder of the pictures
+            $(".imagesDiv").empty()
+            var row = $("<row>")
+            //creating new row tag at index zero
+            for (let index = 0; index < images.length; index++) {
+                //creating row when index is multipul of 3 
+                console.log(index % 3)
+                if (index % 3 === 0) {
+                    var row = $("<row>")
+                }
 
-            })
+                var col = $("<col-sm-3>")
+                var img = $("<img>")
+                img.attr("src", images[index].images.original.url)
+                img.attr("style", "width:25%")
+                col.append(img)
+
+                row.append(col)
+
+                if (index % 3 === 0) {
+                    $(".imagesDiv").append(row)
+                }
+
+            }
+
+        })
     })
 }
 
